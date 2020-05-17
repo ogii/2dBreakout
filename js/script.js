6fcc727ddf704c16ce571c97dbf21a9b79f1c0a0
ogii <ordinallx@gmail.com>
2020-05-17 18:39:15 +0900

    Make the ball bounce when hitting boundary

--- js/script.js
+++ js/script.js
@@ -4,10 +4,11 @@ let x = canvas.width / 2;
 let y = canvas.height - 30;
 let dx = 2;
 let dy = -4 / Math.PI;
+let ballRadius = 15;
 
 const renderBall = () => {
   ctx.beginPath();
-  ctx.arc(x, y, 15, 0, Math.PI * 2);
+  ctx.arc(x, y, ballRadius, 0, Math.PI * 2);
   ctx.fillStyle = "#8a2be2";
   ctx.fill();
   ctx.closePath();
@@ -16,8 +17,15 @@ const renderBall = () => {
 const draw = () => {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   renderBall();
+  if (x + dx > (canvas.width - ballRadius) || x + dx < 0) {
+    dx = -dx;
+  }
+
+  if (y + dy > (canvas.height - ballRadius) || y + dy < 0) {
+    dy = -dy;
+  }
   x += dx;
   y += dy;
 };
 
-let moveBall = setInterval(draw, 20);
+let moveBall = setInterval(draw, 10);
