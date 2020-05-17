70dc4490c20f2197d0fbbf9163edad5b0389e05a
ogii <ordinallx@gmail.com>
2020-05-17 18:55:03 +0900

    Add collision detection with paddle

--- js/script.js
+++ js/script.js
@@ -6,6 +6,30 @@ let dx = 2;
 let dy = -4 / Math.PI;
 let ballRadius = 15;
 
+let paddleHeight = 10;
+let paddleWidth = 75;
+let paddleX = (canvas.width - paddleWidth) / 2;
+let rightPressed = false;
+let leftPressed = false;
+
+const keyDownHandler = (e) => {
+  if (e.key == "Right" || e.key == "ArrowRight") {
+    rightPressed = true;
+  }
+  else if (e.key == "Left" || e.key == "ArrowLeft") {
+    leftPressed = true;
+  }
+};
+
+const keyUpHandler = (e) => {
+  if (e.key == "Right" || e.key == "ArrowRight") {
+    rightPressed = false;
+  }
+  else if (e.key == "Left" || e.key == "ArrowLeft") {
+    leftPressed = false;
+  }
+};
+
 const renderBall = () => {
   ctx.beginPath();
   ctx.arc(x, y, ballRadius, 0, Math.PI * 2);
@@ -14,18 +38,51 @@ const renderBall = () => {
   ctx.closePath();
 };
 
+const renderPaddle = () => {
+  ctx.beginPath();
+  ctx.rect(paddleX, canvas.height - paddleHeight, paddleWidth, paddleHeight);
+  ctx.fillStyle = "#0095DD";
+  ctx.fill();
+  ctx.closePath();
+};
+
 const draw = () => {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   renderBall();
+  renderPaddle();
   if (x + dx > (canvas.width - ballRadius) || x + dx < 0) {
     dx = -dx;
   }
 
-  if (y + dy > (canvas.height - ballRadius) || y + dy < 0) {
+  if (y + dy < ballRadius) {
     dy = -dy;
+  } else if (y + dy > canvas.height - ballRadius) {
+    if (x > paddleX && x < paddleX + paddleWidth) {
+      dy = -dy;
+    }
+    else {
+      document.location.reload();
+      clearInterval(interval);
+    }
   }
+
+  if (rightPressed) {
+    paddleX += 7;
+    if (paddleX + paddleWidth > canvas.width) {
+      paddleX = canvas.width - paddleWidth;
+    }
+  }
+  else if (leftPressed) {
+    paddleX -= 7;
+    if (paddleX < 0) {
+      paddleX = 0;
+    }
+  }
+
   x += dx;
   y += dy;
 };
+document.addEventListener("keydown", keyDownHandler, false);
+document.addEventListener("keyup", keyUpHandler, false);
 
 let moveBall = setInterval(draw, 10);
