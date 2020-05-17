8641576694777ff6900f2d707f8a1653b7845c87
ogii <ordinallx@gmail.com>
2020-05-17 18:22:59 +0900

    Move ball with draw function

---
+++ js/script.js
@@ -0,0 +1,19 @@
+let canvas = document.getElementById('gameCanvas');
+let ctx = canvas.getContext('2d');
+let x = canvas.width / 2;
+let y = canvas.height - 30;
+let dx = 2;
+let dy = -4 / Math.PI;
+
+const draw = () => {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.beginPath();
+  ctx.arc(x, y, 15, 0, Math.PI * 2);
+  ctx.fillStyle = "#8a2be2";
+  ctx.fill();
+  ctx.closePath();
+  x += dx;
+  y += dy;
+};
+
+let moveBall = setInterval(draw, 20);
